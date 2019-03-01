a36414829c1505fa21385c8e5f2d805f25ee0de6
teilon <teiloncw@gmail.com>
2019-03-01 18:31:22 +0600

    correct db.py

--- db.py
+++ db.py
@@ -5,51 +5,69 @@ def db_connet():
     con = postgresql.open('pq://postgres:torn@185.143.173.37:5432/dbmarket')
     test(con)
 
+def fill_dics(con):
+    # formdict
+    con.execute('INSERT INTO formdict (name) VALUES ("market point")')
+    con.execute('INSERT INTO formdict (name) VALUES ("chain point")')
+    con.execute('INSERT INTO formdict (name) VALUES ("sale point")')
+
+    #paymentdict
+    con.execute('INSERT INTO paymentdict (name) VALUES ("cash")')
+    con.execute('INSERT INTO paymentdict(name) VALUES ("real")')
+    con.execute('INSERT INTO paymentdict(name) VALUES ("cons")')
+
 
 def test(con):
     con.execute(
-        'CREATE TABLE formcat(' +
-	    'id SERIAL PRIMARY KEY,' +
-	    'name char(16) NOT NULL CHECK (name <> ''),' +
-	    ')'
+        'CREATE TABLE FormDict(' +
+        'id SERIAL PRIMARY KEY,' +
+        'name char(16) NOT NULL CHECK (name <> ''),' +
+        ')'
     )
     con.execute(
-        'CREATE TABLE paymentcat(' +
-	    'id SERIAL PRIMARY KEY,' +
-	    'name char(16) NOT NULL CHECK (name <> ''),' +
-	    ')'
+        'CREATE TABLE PaymentDict(' +
+        'id SERIAL PRIMARY KEY,' +
+        'name char(16) NOT NULL CHECK (name <> ''),' +
+        ')'
+    )
+    # con.execute(
+    #     'CREATE TABLE countcat(' +
+    #     'id SERIAL PRIMARY KEY,' +
+    #     'name char(16) NOT NULL CHECK (name <> ''),' +
+    #     'description char(64),' +
+    #     ')'
+    # )
+    con.execute(
+        'CREATE TABLE Point(' +
+        'id SERIAL PRIMARY KEY,' +
+        'name char(64) NOT NULL CHECK (name <> ''),' +
+        'form_id integer references formcat(id),' +
+        'payment_id integer references paymentcat(id)' +
+        ')'
     )
     con.execute(
-        'CREATE TABLE countcat(' +
-	    'id SERIAL PRIMARY KEY,' +
-	    'name char(16) NOT NULL CHECK (name <> ''),' +
-	    'description char(64),' +
-	    ')'
+        'CREATE TABLE PointContact(' +
+        'id SERIAL PRIMARY KEY,' +
+        'point_id integer references Point(id),' +
+        'name char(64),' +
+        'contact char(64),' +
+        ')'
     )
     con.execute(
-        'CREATE TABLE point(' +
-	    'id SERIAL PRIMARY KEY,' +
-	    'name char(64) NOT NULL CHECK (name <> ''),' +
-	    'form_id integer references formcat(id),' +
-	    'payment_id integer references paymentcat(id),' +
-	    'count_id integer references countcat(id),' +
-	    ')'
+        'CREATE TABLE Trader(' +
+        'id SERIAL PRIMARY KEY,' +
+        'name char(16) NOT NULL CHECK (name <> ''),' +
+        ')'
     )
     con.execute(
-        'CREATE TABLE trader(' +
-	    'id SERIAL PRIMARY KEY,' +
-	    'name char(16) NOT NULL CHECK (name <> ''),' +
-	    ')'
-    )
-    con.execute(
-        'CREATE TABLE trade(
-	    'id SERIAL PRIMARY KEY,
-	    'trader_id integer references trader(id),
-	    'point_id integer references point(id),
-	    'count integer,
-	    'sale money,
-	    'date DATE NOT NULL DEFAULT CURRENT_DATE
-	    ')'
+        'CREATE TABLE Trade(' +
+        'id SERIAL PRIMARY KEY,' +
+        'trader_id integer references trader(id),' +
+        'point_id integer references point(id),' +
+        'count integer,' +
+        'sale money,' +
+        'date DATE NOT NULL DEFAULT CURRENT_DATE' +
+        ')'
     )
 
 # CREATE TABLE region (
